61163c25d89d706e940c21275a623b284837cc62
gamminghobbit <willhardy00@gmail.com>
2025-04-25 09:46:29 -0600

    complete prep 2.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Please input your grade: ");
+        string percent = Console.ReadLine();
+        int grade = int.Parse(percent);
+        if (grade > 90)
+            {Console.WriteLine("You got an A.");}
+        else if (grade > 80 && grade < 90)
+            {Console.WriteLine("You got a B.");}
+        else if (grade > 70 && grade < 80)
+            {Console.WriteLine("You got a C.");}
+        else if (grade > 60 && grade < 70)
+            {Console.WriteLine("You got a D.");}
+        else if (grade < 60)
+            {Console.WriteLine("You got a F.");}
     }
 }
