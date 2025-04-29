9c6556ef7e935eb88d6b32722edea0b79580300d
gamminghobbit <willhardy00@gmail.com>
2025-04-29 09:36:32 -0600

    in class

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        //04-29-25
+        int x = 10;
+        if (x ==10)
+        {
+            Console.WriteLine("X is 10");
+        }
+        for(int i = 0; i<x; i++)
+        {Console.WriteLine($"Bob is cool: {i}");}
     }
 }
